4ee639466bed4359b03000adef22cd9ba7d7379a
nurtazim <83503579+nurtazim@users.noreply.github.com>
2021-07-05 21:36:10 +0600

    hw

---
+++ product/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from product.models import Category
+from product.models import Reciew
+from product.models import Products
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Reciew)
+admin.site.register(Products)
---
+++ product/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+class Products(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    description = models.TextField(null=True,blank=True,verbose_name="Описание")
+    price = models.FloatField(verbose_name="Цена")
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,verbose_name="Категория")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.title
+
+class Reciew(models.Model):
+    text = models.TextField(verbose_name="Текст")
+    product = models.ForeignKey(Products,
+                                on_delete=models.CASCADE, verbose_name="Продукт")
+    class Meta:
+        verbose_name = "Обзор"
+        verbose_name_plural = "Обзоры"
+
+    def __str__(self):
+        return self.text
+
